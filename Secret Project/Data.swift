9c59953910625b09155556aed9873fdf58414f12
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-15 21:11:27 +0400

    Added some additional data.

--- Secret Project/Data.swift
+++ Secret Project/Data.swift
@@ -8,6 +8,8 @@ class Data {
             data.append(Model(title: "Steve Jobs", subTitle: "Apple", image: UIImage.init(named: "steve"), data1: "Co-Founder", data2: ""))
             data.append(Model(title: "Bill Gates", subTitle: "Microsoft", image: UIImage.init(named: "bill"), data1: "Founder", data2: ""))
             data.append(Model(title: "Mark Zuckerberg", subTitle: "Facebook", image: UIImage.init(named: "mark"), data1: "Co-Founder & CEO", data2: ""))
+            data.append(Model(title: "Sergey Brin", subTitle: "Google", image: UIImage.init(named: "sergey"), data1: "Co-Founder & CEO", data2: ""))
+            data.append(Model(title: "Larry Page", subTitle: "Google", image: UIImage.init(named: "larry"), data1: "Co-Founder & CEO", data2: ""))
             data.append(Model(title: "Kevin Systrom", subTitle: "Instagram", image: UIImage.init(named: "kevin"), data1: "Co-Founder & CEO", data2: ""))
             data.append(Model(title: "Jack Dorsey", subTitle: "Twitter", image: UIImage.init(named: "jack"), data1: "Co-Founder & CEO", data2: ""))
             
