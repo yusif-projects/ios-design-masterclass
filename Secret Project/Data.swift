419890d0f7929248e292853a173281eef5847596
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-15 20:20:31 +0400

    Added some images and some data.

--- Secret Project/Data.swift
+++ Secret Project/Data.swift
@@ -5,7 +5,11 @@ class Data {
     static func getData(completion: @escaping ([Model]) -> ()) {
         DispatchQueue.global(qos: .userInteractive).async {
             var data = [Model]()
-            data.append(Model(title: "Title", subTitle: "Subtitle", image: nil, data1: "Data1", data2: "Data2"))
+            data.append(Model(title: "Steve Jobs", subTitle: "Apple", image: UIImage.init(named: "steve"), data1: "Co-Founder", data2: ""))
+            data.append(Model(title: "Bill Gates", subTitle: "Microsoft", image: UIImage.init(named: "bill"), data1: "Founder", data2: ""))
+            data.append(Model(title: "Mark Zuckerberg", subTitle: "Facebook", image: UIImage.init(named: "mark"), data1: "Co-Founder & CEO", data2: ""))
+            data.append(Model(title: "Kevin Systrom", subTitle: "Instagram", image: UIImage.init(named: "kevin"), data1: "Co-Founder & CEO", data2: ""))
+            data.append(Model(title: "Jack Dorsey", subTitle: "Twitter", image: UIImage.init(named: "jack"), data1: "Co-Founder & CEO", data2: ""))
             
             DispatchQueue.main.async {
                 completion(data)
