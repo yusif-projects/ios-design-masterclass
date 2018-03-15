318709ecae350e5edc45a21803e033457790c1d0
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-15 22:01:59 +0400

    Animation works.

--- Secret Project/ViewController.swift
+++ Secret Project/ViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var menu_view: UIViewX!
     @IBOutlet weak var tableView: UITableView!
     
     var tableData: [Model] = []
@@ -14,6 +15,18 @@ class ViewController: UIViewController {
         Data.getData { (data) in
             self.tableData = data
             self.tableView.reloadData()
+        }
+        
+        menu_view.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+    }
+    
+    @IBAction func button_pressed(_ sender: Floating_Action_Button) {
+        UIView.animate(withDuration: 0.3) {
+            if self.menu_view.transform == .identity {
+                self.menu_view.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            } else {
+                self.menu_view.transform = .identity
+            }
         }
     }
 }
